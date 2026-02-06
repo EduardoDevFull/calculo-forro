db80f82927e0eba2b64637db15ea2ebfb8f6a7b2
EduardoDevFull <78571302+EduardoDevFull@users.noreply.github.com>
2026-02-06 20:12:28 -0300

    Refactor service worker for caching enhancements
    Updated service worker to use a new cache name and include additional files for caching.

--- sw.js
+++ sw.js
@@ -1,10 +1,37 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('forro-pvc').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-      ]);
+const CACHE_NAME = 'forro-pvc-v2';
+
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icone.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
